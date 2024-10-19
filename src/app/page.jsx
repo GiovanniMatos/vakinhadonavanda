0c2d7178ac0edc3a1467d7a2358c08fe0b9c4c2d
GiovanniMatos <giovannyafonso23@gmail.com>
2024-10-19 15:28:32 -0300

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,10 +13,10 @@ export default function Home() {
     <div className="container mx-auto mt-8 text-center">
       <h1 className="mb-2 text-sm">SAÚDE / TRATAMENTOS</h1>
       <h1 className="lg:text-5xl text-2xl font-bold mb-2">Ajuda no tratamento da minha avó (Vanda)</h1>
-      <h1 className="lg:mb-8 mb-3 lg:text-lg">ID: 5136040</h1>
+      <h1 className="lg:mb-8 mb-3 lg:text-lg pr-">ID: 5136040</h1>
       <DonateContent />
       <PayButton href="/contribua" textButton="Quero Ajudar"
-       paddingLeft="pl-36" paddingRight="pr-36" marginBottom="mb-7"/>
+       paddingLeft="pl-32" paddingRight="pr-32" marginBottom="mb-7"/>
       <NavContent />
     </div>
     </>
