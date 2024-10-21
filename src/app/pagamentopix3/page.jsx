6eafa4e9c7ba5b25f3182047ec13216b516ec391
Giovanny Matos <99231397+GiovanniMatos@users.noreply.github.com>
2024-10-21 13:25:17 -0400

    Update page.jsx

--- src/app/pagamentopix3/page.jsx
+++ src/app/pagamentopix3/page.jsx
@@ -9,7 +9,7 @@ export default function HomePagamento(){
         <div className="container mx-auto text-center mt-5">
             <h1 className="lg:text-5xl text-2xl font-bold mb-2">Efetue o pagamento para
             confirmar a contribuição</h1>
-            <h1 className="text-lg mt-3">Você gostaria de contribuir com <span className="text-green-400 font-bold">R$ 50,00</span> :)</h1>
+            <h1 className="text-lg mt-3">Você gostaria de contribuir com <span className="text-green-400 font-bold">R$ 100,00</span> :)</h1>
             <img src="pix100reais.jpg" className="mt-2 bg-gray-800 w-[15rem] h-[15rem] mx-auto text-white font-bold"/>
             <CopyLink PixCode="00020126580014br.gov.bcb.pix0136ad777dcb-ab7a-4df7-9424-01287cc1bbde5204000053039865406100.005802BR5924Giovanny de Melo Afonso 6008Brasilia62090505almqx6304D691"
             defaultValue="00020126580014br.gov.bcb.pix0136ad777dcb-ab7a-4df7-9424-01287cc1bbde5204000053039865406100.005802BR5924Giovanny de Melo Afonso 6008Brasilia62090505almqx6304D691"/>
