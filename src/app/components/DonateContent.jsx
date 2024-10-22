e9e22f9732409f6235dd3792b0b86e726137b35d
Giovanny Matos <99231397+GiovanniMatos@users.noreply.github.com>
2024-10-22 11:21:59 -0400

    Update DonateContent.jsx

--- src/app/components/DonateContent.jsx
+++ src/app/components/DonateContent.jsx
@@ -6,7 +6,6 @@ export default function DonateContent(){
         <div className="ml-4 lg:flex justify-between">
             <div className="mb-4 lg:ml-32">
                 <img src="fotovanda1.jpg" className="lg:w-[44rem] lg:h-[26rem] w-[28rem] bg-slate-600"/>
-                <p className="text-left text-sm mt-5 mb-4 lg:mb-4">Seja um dos colaboradores desta vaquinha! <FaHeart color="red"/></p>
                 <div className="flex">
                     <div className="bg-blue-500 pt-2 pb-2 w-12 rounded-full mr-1">
                         <h1 className="text-white font-bold mt-1">GM</h1>
