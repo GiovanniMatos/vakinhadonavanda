85412ac7132bf3e6d7ef6a86495d5422530f7f2f
Giovanny Matos <99231397+GiovanniMatos@users.noreply.github.com>
2024-11-05 19:37:23 -0400

    Update NavContribuidores.jsx

--- src/app/components/DonateContent/NavDonateContent/NavContribuidores.jsx
+++ src/app/components/DonateContent/NavDonateContent/NavContribuidores.jsx
@@ -4,7 +4,7 @@ export default function NavContribuidores(){
     return(
         <div className="text-sm left w-[24rem]">
             <h2 className="bg-slate-100 pt-4 pb-4 text-center text-base font-bold rounded-md">Jhemersson Ferreira</h2>
-            <h2 className="bg-slate-100 pt-4 pb-4 text-center text-base font-bold rounded-md">Kaylla Karol</h2>
+            <h2 className="bg-slate-100 pt-4 pb-4 text-center text-base font-bold rounded-md">Filipe Dantas Martins</h2>
             <h2 className="bg-slate-100 pt-4 pb-4 text-center text-base font-bold rounded-md">Gustavo Augusto</h2>
             <h2 className="bg-slate-100 pt-4 pb-4 text-center text-base font-bold rounded-md">Marcos Victor Santos</h2>
             <h2 className="bg-slate-100 pt-4 pb-4 text-center text-base font-bold rounded-md">Wesley Fernandes</h2>
